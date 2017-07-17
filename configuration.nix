b51d2a532b02242ea4e276ed1d01cf839f7e92cc
Rene <info@fland.re>
2017-07-17 11:36:47 +0200

    Fixed symlink

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,69 @@
-/etc/nixos/configuration.nix
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda"; 
+  networking.hostName = "nixos"; 
+  networking.wireless.enable = true; 
+
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  time.timeZone = "Europe/Amsterdam";
+
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    wget
+    git
+    links
+    vim
+    firefox
+  ];
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # services.printing.enable = true;
+
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e";
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  users.extraUsers.rene = {
+    isNormalUser = true;
+    group = "rene";
+    name = "rene";
+    extraGroups = [
+	"users"
+	"wheel"
+	"networkmanager"
+	"audio"
+     ];
+    uid = 1000;
+    home = "/home/rene/";
+    createHome = true;
+  };
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "17.03";
+
+}
